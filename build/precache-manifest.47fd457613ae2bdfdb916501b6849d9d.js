c5dcabc49adc34645b39c4056ab3bc395c2a33b0
Antkeo1 <sleav143@gmail.com>
2019-03-20 12:42:06 -0400

    searchbar component created

---
+++ build/precache-manifest.47fd457613ae2bdfdb916501b6849d9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f092f99dfdea401601a",
+    "url": "/VideoApp/static/js/runtime~main.9f092f99.js"
+  },
+  {
+    "revision": "aab1f33ceb6973f8345f",
+    "url": "/VideoApp/static/js/main.aab1f33c.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/VideoApp/static/js/2.ae0b72b8.chunk.js"
+  },
+  {
+    "revision": "576b13924d4d71e6b523ea958bc32310",
+    "url": "/VideoApp/index.html"
+  }
+];
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = {
+    term: ''
+  }
+
+  // event handler
+  termChange = (e) => {
+    this.setState({
+      term: e.target.value
+    })
+  }
+
+  formSubmit = e => {
+    e.preventDefault()
+    
+  }
+
   render() {
     return (
-      <div>SearchBar</div>
+      <div className='searchbar ui segment'>
+        <form onSubmit={this.formSubmit} className='ui form' >
+          <div className='field'>
+            <label>Video Search</label>
+            <input type='text'
+                    value={this.state.term}
+                    onChange={this.termChange}
+                  />
+          </div>
+        </form>
+      </div>
     )
   }
 }
