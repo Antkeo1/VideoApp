42897b939cadc9f1cf1d19d05abf60dae6bdf2c5
Antkeo1 <sleav143@gmail.com>
2019-03-21 12:09:41 -0400

    added some css to videoitem

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,7 +1,18 @@
+import './VideoItem.css'
 import React from 'react'
 
-const VideoItem = props => {
-  return <div>VideoItem</div>
+const VideoItem = ({ video }) => {
+  return (
+    <div className='video-item item'>
+      <img alt='' className='ui image' src={video.snippet.thumbnails.medium.url} />
+      <div className='content'>
+        <div className='header'>
+          {video.snippet.title}
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default VideoItem
