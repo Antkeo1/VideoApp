5764955bcbfa709a5a27ae753bd98c8a0f34b206
Antkeo1 <sleav143@gmail.com>
2019-03-21 08:17:21 -0400

    created api cred from youtube api

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyDkJJz6fCuAkKG3HVnFnnmp-3jIAmUV0O4'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResult: 5,
+    key: KEY
+  }
+})
